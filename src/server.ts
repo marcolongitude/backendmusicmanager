797c22e574f7a1b48009f852710634d54b7acbe3
marcolongitude <marcocpdti@gmail.com>
2022-06-05 11:39:15 -0300

    configuration server express

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
+import { router } from './routes'
 
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(router);
 app.listen(3000, () => console.log("Server is running on port 3000"));
