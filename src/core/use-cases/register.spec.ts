304510a52788357ee5f6e6da8a04bef498f8222f
marcolongitude <marcocpdti@gmail.com>
2022-06-05 11:41:26 -0300

    configurations

--- src/core/use-cases/register.spec.ts
+++ src/core/use-cases/register.spec.ts
@@ -1,9 +1,25 @@
-import {Register} from './register'
+import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import {OutsideRegister, register} from './users';
+import { User } from '../types/user';
 
-const response = Register();
 
-describe('Test user functions Group', () => {
-    it('test initial', () => {
-        expect(response).toBe('teste')
-    })
+const registerOk: OutsideRegister<string> = async (data) => {
+    return `Usuário ${data.name} cadastrado com sucesso!`
+}
+
+const data: User = {
+    id: 'fsalkdflksdf',
+    name: 'marco',
+    email: 'marco@email.com',
+    avatar: 'alsdkflskdfk'
+}
+
+it('Deveria cadastrar um usuário com sucesso', async () => {
+    return pipe(
+        data,
+        register(registerOk),
+        TE.map(result => expect(result).toBe(`Usuário ${data.name} cadastrado com sucesso!`))
+    )()
 })
